eb218ff9a90e2f3cb60c6db41b794f506e706106
kekehua <100812740+kekehua@users.noreply.github.com>
2022-08-30 20:18:37 -0500

    modified server files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const notes = require("./db/db.json");
 const path = require('path');
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 app.use(express.json());
